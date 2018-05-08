8fa8f99cd39e42b215febbb17ef55538ea2b3940
arroz@dgtl <arrozcunanan@DGTLim-iMac-02.local>
2018-05-08 14:39:38 +0800

    adding users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  private datas  = 
+[
+{  id: "1",
+    name: "Arroz",
+    age: 22 },
+{  id: "2",
+    name: "Jayson",
+    age: 22 },
+{  id: "3",
+    name: "Jarryd",
+    age: 23 },
+{  id: "4",
+    name: "Carlos",
+    age: 26 }
+];
 
   constructor() { }
 
